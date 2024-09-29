23333c68188d4a6acbe814e408886e4276277988
Simon Nguyen <80842534+cobeo2004@users.noreply.github.com>
2024-09-29 23:28:02 +1000

    final assignment 2 commit

--- Assignment2/conda-config.sh
+++
@@ -1,9 +0,0 @@
-# Create a new conda environment
-conda create -n cos30049_env python=3.10.9
-conda activate cos30049_env
-
-# Check current environment
-conda info --envs
-
-#Check current python version
-python --version
