1c0741c7f7f28e7f0dfc0a974f7e8e136f6657f9
vtank4 <vietanhvt2004@gmail.com>
2024-10-30 19:26:56 +1100

    vank dev

--- client-next/src/server/ping.ts
+++ client-next/src/server/ping.ts
@@ -4,6 +4,7 @@ import { API_URL } from "@/lib/constant";
 import { redirect } from "next/navigation";
 
 export const ping = async () => {
+  console.log("API_URL", API_URL);
   const result = await fetch(`${API_URL}/ping`);
   if (!result.ok) {
     redirect("/errors/not-ping");
